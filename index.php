1af2329213838c8c6aa0906b1fa108bc473f39a5
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2013-09-09 21:55:10 +0200

    Initial commit. 2013 photos, static two-part webpage.

---
+++ index.php
@@ -0,0 +1,65 @@
+<style type="text/css">
+body {
+	margin: 0;
+	padding: 0;
+	font-family: sans-serif;
+	background: green;
+}
+#left, #right {
+	min-height: 100%;
+	width: 50%;
+	display: inline-block;
+	background: green no-repeat center center fixed; 
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+	vertical-align: top;
+}
+.text {
+	background: rgba(255, 255, 255, 0.5);
+	padding: 1em;
+	margin: 1em;
+}
+#left {
+	background-image: url('dauw1.jpg');
+}
+#right {
+	background-image: url('dauw2.jpg');
+}
+h1, h2 { 
+	color: white;
+	padding: 0 1em;
+}
+#right h1, #right h2 {
+	text-align: right;
+}
+</style>
+<div id="left">
+<h1>2013</h1>
+<h2>9 mei 2013</h2>
+<div class="text">
+<iframe src="http://www.gpsvisualizer.com/display/1368107553-25124-31.149.72.190.html" width="100%" height="500px"></iframe>
+</div>
+</div><div id="right">
+<h1>2014</h1>
+<h2>29 mei 2014</h2>
+<div class="text">
+<p>
+Het is alweer bijna Hemelvaartsdag, dus wordt het weer tijd om
+condens te gaan vertrappen, ofwel dauwtrappen, zoals dat ook wel
+heet.
+</p><p>
+Traditiegetrouw organiseren de explo's al vele jaren dit grandioze
+wandel- en eetfestijn, wat altijd als een succes uitpakt. Gegarandeerd
+geen saaie wandeling met een boterhammetje uit een zakje, maar een
+mooie tocht met als afsluiter een zeer uitgebreide brunch op het
+clubhuis. Explo's noemen het daarom ook wel de Dawnhike.
+</p><p>
+Deze tocht is tevens een geweldige mogelijkheid voor je ouders,
+vrienden, familie of buren om een snufje van scouting op te pikken.
+Deze tocht is namelijk voor iedereen!
+</p>
+</div>
+
+</div>
